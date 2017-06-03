f3f100fa66b3e8b2332f0a969e387c6d8e8e017a
yaoqijun <yaoqijunmail@gmail.com>
2017-06-03 12:44:01 +0800

    init config

--- config-server/src/main/java/org/yqj/config/server/demo/BootDemoApplication.java
+++ config-server/src/main/java/org/yqj/config/server/demo/BootDemoApplication.java
@@ -2,11 +2,7 @@ package org.yqj.config.server.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by yaoqijun.
@@ -15,18 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Descirbe:
  */
 @SpringBootApplication
-@Controller
+@EnableConfigServer
 public class BootDemoApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
                 .banner(new DemoBanner())
                 .sources(BootDemoApplication.class)
                 .run(args);
-    }
-
-    @RequestMapping(value = "/index", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String indexPage(){
-        return "this is test index paging info";
     }
 }
