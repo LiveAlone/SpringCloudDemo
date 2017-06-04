6f8118dc5141007c428b185b1094b1b5e4013604
yaoqijun <yaoqijunmail@gmail.com>
2017-06-04 12:59:16 +0800

    eureka start

---
+++ eureka-client/src/main/java/org/yqj/config/server/demo/BootDemoApplication.java
@@ -0,0 +1,36 @@
+package org.yqj.config.server.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-04-27
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class BootDemoApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .banner(new DemoBanner())
+                .sources(BootDemoApplication.class)
+                .run(args);
+    }
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping(value = "hi")
+    public String home(@RequestParam String name){
+        return  "hi " + name + " i am from port: " + port;
+    }
+
+}
