e84fbd5df6eabaf21f9f5a4fbb2921bd2c80759b
yaoqijun <yaoqijunmail@gmail.com>
2017-06-04 17:15:37 +0800

    add all

---
+++ zuul-service/src/main/java/org/yqj/zuul/service/demo/BootDemoApplication.java
@@ -0,0 +1,24 @@
+package org.yqj.zuul.service.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-04-27
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class BootDemoApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .banner(new DemoBanner())
+                .sources(BootDemoApplication.class)
+                .run(args);
+    }
+}
