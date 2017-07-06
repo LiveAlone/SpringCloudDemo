fb2d132cb9d52bb4ae74dccaec783ca4b8aecd5c
yaoqijun <yaoqijun@changingedu.com>
2017-07-06 14:27:43 +0800

    添加Single 的配置方式

---
+++ config-client-local/src/main/java/org/yqj/config/client/local/demo/BootDemoApplication.java
@@ -0,0 +1,42 @@
+package org.yqj.config.client.local.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-04-27
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+@SpringBootApplication
+@Controller
+public class BootDemoApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .banner(new DemoBanner())
+                .sources(BootDemoApplication.class)
+                .run(args);
+    }
+
+    @Value("${foo}")
+    private String fooRemoteConfig;
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String indexPage(){
+        return "this is test index paging info";
+    }
+
+    @RequestMapping(value = "/foo/config", method = RequestMethod.GET)
+    @ResponseBody
+    public String configDetail(){
+        return fooRemoteConfig;
+    }
+}
---
+++ config-server-local/src/main/java/org/yqj/config/server/local/demo/BootDemoApplication.java
@@ -0,0 +1,22 @@
+package org.yqj.config.server.local.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-04-27
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class BootDemoApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .banner(new DemoBanner())
+                .sources(BootDemoApplication.class)
+                .run(args);
+    }
+}
