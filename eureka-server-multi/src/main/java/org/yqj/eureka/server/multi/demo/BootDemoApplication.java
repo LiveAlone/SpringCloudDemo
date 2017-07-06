361bdd9d18a094e8befbfbf493703fbbec30fe0f
yaoqijun <yaoqijunmail@gmail.com>
2017-07-07 02:07:51 +0800

    add all config

---
+++ eureka-server-multi/src/main/java/org/yqj/eureka/server/multi/demo/BootDemoApplication.java
@@ -0,0 +1,22 @@
+package org.yqj.eureka.server.multi.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-04-27
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class BootDemoApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .banner(new DemoBanner())
+                .sources(BootDemoApplication.class)
+                .run(args);
+    }
+}
